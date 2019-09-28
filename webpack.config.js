69ca2e6aa9ed8fe976fc6b4c931c74e10179ad77
Colin VanLang <colinvanlang@gmail.com>
2019-09-28 16:20:09 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const { TypedCssModulesPlugin } = require('typed-css-modules-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  output: {
+    path: path.resolve(__dirname, 'static'),
+    filename: 'bundle.js',
+    publicPath: '/static',
+  },
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx', '.css'],
+    modules: [path.resolve(__dirname, 'src'), 'node_modules']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader',
+      },
+      {
+        test: /\.css$/,
+        loader: 'css-loader',
+        options: {
+          modules: true,
+          localIdentName: '[name]__[local]__[hash:base64]',
+        },
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'initial',
+      cacheGroups: {
+        vendors: {
+          test: /[\\/]node_modules[\\/]/,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
+  plugins: [
+    new TypedCssModulesPlugin({
+      globPattern: 'src/**/*.css',
+    }),
+  ],
+}
